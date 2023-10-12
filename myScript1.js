7d4644732960396730a53bf79facaa922993a270
ahmednaser89 <ahmednaser89@users.noreply.github.com>
2023-10-13 00:26:01 +0300

    Create myScript1.js

---
+++ myScript1.js
@@ -0,0 +1,5 @@
+    <script src="https://thunkable.github.io/webviewer-extension/thunkableWebviewerExtension.js" type="text/javascript"></script>
+ThunkableWebviewerExtension.receiveMessage(function(message) {
+  // Do something with your message
+  alert(message)
+});
